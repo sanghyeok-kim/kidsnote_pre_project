6df2033b922f5d6109fda5726a4d485f05b377d1
sanghyeok-kim <slsnsi1212@gmail.com>
2023-11-04 17:28:27 +0900

    #8 feat: BookEntity 구현

---
+++ Kidsnote_Pre_Project/Kidsnote_Pre_Project/Sources/Core/CoreEntity/BookEntity.swift
@@ -0,0 +1,24 @@
+//
+//  BookEntity.swift
+//  Kidsnote_Pre_Project
+//
+//  Created by 김상혁 on 2023/11/04.
+//
+
+import Foundation
+
+struct BookEntity {
+    let id: String
+    let title: String
+    let authors: [String]
+    let publishedDate: String?
+    let description: String?
+    let isbn13Identifier: String?
+    let pageCount: Int?
+    let shareURL: String?
+    let smallThumbnailURL: String?
+    let thumbnailURL: String?
+    let isEbook: Bool?
+    let buyLink: String?
+    let sampleURL: String?
+}
