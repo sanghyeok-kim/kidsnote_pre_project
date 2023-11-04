6eb070c0b0f67366063e14206c6e83341b0b075d
sanghyeok-kim <slsnsi1212@gmail.com>
2023-11-04 17:25:20 +0900

    #8 feat: 책 검색 API 요청에 대한 결과를 디코딩하는 DTO 타입 구현

---
+++ Kidsnote_Pre_Project/Kidsnote_Pre_Project/Sources/Core/CoreNetwork/DTO/BookSearchResultDTO.swift
@@ -0,0 +1,58 @@
+//
+//  BookSearchResultDTO.swift
+//  Kidsnote_Pre_Project
+//
+//  Created by 김상혁 on 2023/11/03.
+//
+
+import Foundation
+
+struct BookSearchResultDTO: Decodable {
+    let kind: String
+    let totalItems: Int
+    let items: [BookDTO]
+}
+
+extension BookSearchResultDTO {
+    struct BookDTO: Decodable {
+        let kind: String?
+        let id: String
+        let etag: String?
+        let volumeInfo: VolumeInfo?
+        let saleInfo: SaleInfo?
+        let accessInfo: AccessInfo?
+    }
+}
+
+extension BookSearchResultDTO.BookDTO {
+    struct VolumeInfo: Decodable {
+        let title: String?
+        let authors: [String]?
+        let publishedDate, description: String?
+        let industryIdentifiers: [IndustryIdentifier]?
+        let pageCount: Int?
+        let canonicalVolumeLink: String?
+        let imageLinks: ImageLinks?
+    }
+
+    struct SaleInfo: Decodable {
+        let isEbook: Bool?
+        let buyLink: String?
+    }
+
+    struct AccessInfo: Decodable {
+        let webReaderLink: String?
+        let accessViewStatus: String?
+    }
+}
+
+extension BookSearchResultDTO.BookDTO.VolumeInfo {
+    struct ImageLinks: Decodable {
+        let smallThumbnail, thumbnail: String?
+    }
+    
+    struct IndustryIdentifier: Decodable {
+        let type: String?
+        let identifier: String?
+    }
+}
