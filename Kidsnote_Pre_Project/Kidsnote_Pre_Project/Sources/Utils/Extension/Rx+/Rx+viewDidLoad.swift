bac212161e7712e1dcecbcdf9dbd515b94554813
sanghyeok-kim <slsnsi1212@gmail.com>
2023-11-05 20:14:53 +0900

    #14 feat: Rx+viewDidLoad 생성 및 구현

---
+++ Kidsnote_Pre_Project/Kidsnote_Pre_Project/Sources/Utils/Extension/Rx+/Rx+viewDidLoad.swift
@@ -0,0 +1,18 @@
+//
+//  Rx+viewDidLoad.swift
+//  Kidsnote_Pre_Project
+//
+//  Created by 김상혁 on 2023/11/04.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+public extension Reactive where Base: UIViewController {
+    var viewDidLoad: Observable<Void> {
+        let targetSelector = #selector(Base.viewDidLoad)
+        return sentMessage(targetSelector).map { _ in }
+    }
+}
