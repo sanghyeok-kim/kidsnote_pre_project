0c4e53e12feb2d1fdb0ca8bfa244a7460bfdbfeb
sanghyeok-kim <slsnsi1212@gmail.com>
2023-11-05 20:07:43 +0900

    #14 remove: 초기 ViewController 삭제

--- Kidsnote_Pre_Project/Kidsnote_Pre_Project/Sources/App/SceneDelegate.swift
+++ Kidsnote_Pre_Project/Kidsnote_Pre_Project/Sources/App/SceneDelegate.swift
@@ -15,10 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        let initialVC = ViewController()
         AppDIContainer.shared.registerDependencies()
         
-        window?.rootViewController = initialVC
         window?.makeKeyAndVisible()
     }
 }
