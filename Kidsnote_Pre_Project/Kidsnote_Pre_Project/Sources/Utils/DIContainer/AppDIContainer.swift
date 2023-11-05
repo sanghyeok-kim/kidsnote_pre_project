4ca6f6b234e58c3fa04a530324326ecde591d2be
sanghyeok-kim <slsnsi1212@gmail.com>
2023-11-05 19:23:11 +0900

    #12 feat: AppDIContainer에 필요한 의존성 객체 주입

--- Kidsnote_Pre_Project/Kidsnote_Pre_Project/Sources/Utils/DIContainer/AppDIContainer.swift
+++ Kidsnote_Pre_Project/Kidsnote_Pre_Project/Sources/Utils/DIContainer/AppDIContainer.swift
@@ -13,11 +13,28 @@ final class AppDIContainer: DIContainer {
     var storage: [String: Any] = [:]
     
     private init() { }
-    
 }
 
 extension AppDIContainer {
     func registerDependencies() {
+        // Core
+        AppDIContainer.shared.register(service: URLSessionNetworkService.self) {
+            URLSessionNetworkService.shared
+        }
         
+        // DataMapper
+        AppDIContainer.shared.register(service: AnyDataMapper.self) {
+            AnyDataMapper(BookEntityMapper())
+        }
+        
+        // Repository
+        AppDIContainer.shared.register(service: SearchBookRepository.self) {
+            DefaultSearchBookRepository()
+        }
+        
+        // UseCase
+        AppDIContainer.shared.register(service: SearchBookUseCase.self) {
+            DefaultSearchBookUseCase()
+        }
     }
 }
