1e5e3b148bf73bc8b4053321c7306f1be53ebc0d
sanghyeok-kim <slsnsi1212@gmail.com>
2023-11-07 18:12:46 +0900

    #18 feat: ImageAsset 열거형 생성 및 앱에 사용되는 UIImage에 대한 정적 타입 선언

---
+++ Kidsnote_Pre_Project/Kidsnote_Pre_Project/Sources/DesignSystem/Asset/ImageAsset.swift
@@ -0,0 +1,13 @@
+//
+//  ImageAsset.swift
+//  Kidsnote_Pre_Project
+//
+//  Created by 김상혁 on 2023/11/07.
+//
+
+import UIKit
+
+enum ImageAsset {
+    static let emptyBookCover = UIImage(named: "EmptyBookCover")
+    static let exclamationmarkTriangle = UIImage(systemName: "exclamationmark.triangle")
+}
