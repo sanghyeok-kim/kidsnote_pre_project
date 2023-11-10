3b658c2e0b0ed5556f1893f943617ba424d80608
sanghyeok-kim <slsnsi1212@gmail.com>
2023-11-10 10:35:39 +0900

    #28 refactor: UIImage의 resizedAspectFit 메소드를 UIGraphicsImageRenderder 사용하도록 변경

--- Kidsnote_Pre_Project/Kidsnote_Pre_Project/Sources/DesignSystem/Extension/UIImage+.swift
+++ Kidsnote_Pre_Project/Kidsnote_Pre_Project/Sources/DesignSystem/Extension/UIImage+.swift
@@ -8,15 +8,19 @@
 import UIKit
 
 extension UIImage {
-    func resizeAspectFit(width: CGFloat) -> UIImage? {
-        defer { UIGraphicsEndImageContext() }
+    func resizedAspect(width newWidth: CGFloat) -> UIImage? {
+        let scale = newWidth / self.size.width
+        let newHeight = self.size.height * scale
         
-        let aspectSize = CGSize(width: width, height: ceil(width / size.width * size.height))
-        
-        UIGraphicsBeginImageContextWithOptions(aspectSize, false, UIScreen.main.scale)
-        draw(in: CGRect(origin: .zero, size: aspectSize))
-        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
-        
+        let newSize = CGSize(width: newWidth, height: newHeight)
+        return resizeAspectFit(to: newSize)
+    }
+    
+    private func resizeAspectFit(to newSize: CGSize) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: newSize)
+        let resizedImage = renderer.image { _ in
+            draw(in: CGRect(origin: .zero, size: newSize))
+        }
         return resizedImage
     }
 }
