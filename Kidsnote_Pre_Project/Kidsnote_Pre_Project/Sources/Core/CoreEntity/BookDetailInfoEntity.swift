6b6bb92d6ce55fe635bc18c0ea6a1db7e6dc98dd
sanghyeok-kim <slsnsi1212@gmail.com>
2023-11-10 20:07:40 +0900

    #28 feat: BookDetailInfoEntity, DTO, Mapper 타입 구현

---
+++ Kidsnote_Pre_Project/Kidsnote_Pre_Project/Sources/Core/CoreEntity/BookDetailInfoEntity.swift
@@ -0,0 +1,12 @@
+//
+//  BookDetailInfoEntity.swift
+//  Kidsnote_Pre_Project
+//
+//  Created by 김상혁 on 2023/11/10.
+//
+
+import Foundation
+
+struct BookDetailInfoEntity {
+    let description: String
+}
