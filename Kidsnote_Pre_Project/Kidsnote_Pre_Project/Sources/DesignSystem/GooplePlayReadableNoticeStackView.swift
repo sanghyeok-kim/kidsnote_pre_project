bc2e9076fcfd586a94be259737b33a839ec7c138
sanghyeok-kim <slsnsi1212@gmail.com>
2023-11-10 20:21:47 +0900

    #28 feat: GooglePlayReadableNoticeStackView 생성 및 구현

---
+++ Kidsnote_Pre_Project/Kidsnote_Pre_Project/Sources/DesignSystem/GooplePlayReadableNoticeStackView.swift
@@ -0,0 +1,64 @@
+//
+//  GooplePlayReadableNoticeStackView.swift
+//  Kidsnote_Pre_Project
+//
+//  Created by 김상혁 on 2023/11/10.
+//
+
+import UIKit
+
+final class GooplePlayReadableNoticeStackView: UIView {
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "info.circle")?.withTintColor(.darkGray, renderingMode: .alwaysOriginal)
+        return imageView
+    }()
+    
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Google Play 웹사이트에서 구매한 책을 이 앱에서 읽을 수 있습니다."
+        label.font = UIFont.systemFont(ofSize: 12.5)
+        label.textColor = .darkGray
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            iconImageView,
+            infoLabel,
+        ])
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.spacing = 12
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layoutUI()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Layout UI
+
+private extension GooplePlayReadableNoticeStackView {
+    func layoutUI() {
+        addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        iconImageView.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        iconImageView.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        stackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+    }
+}
+
